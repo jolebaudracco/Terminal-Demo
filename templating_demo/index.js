29b51ad17a17b85539bd39de3e3a2fdae137a7b1
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-02-17 11:37:00 -0300

    general modification

--- templating_demo/index.js
+++ templating_demo/index.js
@@ -3,28 +3,29 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    res.render('home.ejs')
+    res.render('home')
 })
 
 app.get('/cats', (req, res) => {
     const cats = [
-      'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'  
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
     ]
     res.render('cats', { cats })
 })
 
-app.get('/r/:subreddit', (req, res) =>{
+app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     const data = redditData[subreddit];
-    if(data) {
+    if (data) {
         res.render('subreddit', { ...data });
     } else {
-        res.render('NotFound', { subreddit })
+        res.render('notfound', { subreddit })
     }
 })
 
@@ -34,5 +35,5 @@ app.get('/rand', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log('Listening on Port 3000')
+    console.log("LISTENING ON PORT 3000")
 })
